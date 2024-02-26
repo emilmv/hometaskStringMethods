c95e8a8124cf1f403f0a63ceb18d66276887c70f
emilmv <emilnm@code.edu.az>
2024-02-27 00:38:09 +0400

    Add project files.

---
+++ hometaskStringMethods/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace hometaskStringMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //correctFormat:
+            //Console.WriteLine("Calculator:");
+            //Console.WriteLine("==============");
+            //Console.WriteLine("Ilk ededi daxil edin:");
+            //bool isNumber=double.TryParse(Console.ReadLine(),out double num);
+            //if (!isNumber)
+            //{
+            //    Console.WriteLine("Duzgun eded daxil edin");
+            //    Console.WriteLine("=======================");
+            //    goto correctFormat;
+            //}
+
+            //Console.WriteLine("========================");
+            //Console.WriteLine("Ikinci ededi daxil edin:");
+            //if (!isNumber)
+            //{
+            //    Console.WriteLine("Duzgun eded daxil edin");
+            //    Console.WriteLine("=======================");
+            //    goto correctFormat;
+            //}
+            //bool isNumber2=double.TryParse(Console.ReadLine(),out double num2);
+
+            //Console.WriteLine("=========================");
+            //Console.WriteLine("Icra etmek istediyiniz emeliyyatin simvolunu daxil edin:");
+            //char symbol=Convert.ToChar(Console.ReadLine());
+
+
+
+            //calculator(num, num2, symbol);
+            #endregion
+
+
+        }
+        static void calculator(double num, double num2, char symbol)
+        {
+            switch (symbol)
+            {
+                case '/':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Cavab: 0-a bolmek mumkun deyil");
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Cavab: " + num / num2);
+                    break;
+                case '*':
+                    Console.WriteLine("Cavab: " + num * num2);
+                    break;
+                case '+':
+                    Console.WriteLine("Cavab: " + (num + num2));
+                    break;
+                case '-':
+                    Console.WriteLine("Cavab: " + (num - num2));
+                    break;
+                default:
+                    Console.WriteLine("Duzgun simvol daxil edin");                  
+                    break;
+
+            }
+        }
+
+
+
+    }
+}
